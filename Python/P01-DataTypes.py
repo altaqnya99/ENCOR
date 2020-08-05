bd066e37a4783332851ab12e3ccde79777d3752c
APE-360 Network <68269026+APE360@users.noreply.github.com>
2020-08-05 07:25:23 +0300

    Create P01-DataTypes.py

---
+++ Python/P01-DataTypes.py
@@ -0,0 +1,9 @@
+Router_List=["RA","RB","RC"]
+Router_Tuple=("RA","RB","RC")
+Router_Dic={"RA":"192.168.1.1","RB":"192.168.1.2","RC":"192.168.1.3"}
+
+Router_List.append("RD")
+#Router_Tuple.append("RD")
+
+#print(Router_List)
+print(Router_Dic["192.168.1.1"])
